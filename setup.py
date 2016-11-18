68aecf2a3bc3c1db55fa0608009de5a2f8e740db
edussilva <edussilva@live.com>
2016-11-18 19:59:22 -0200

    app criada

---
+++ setup.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+from setuptools import setup
+
+
+setup(
+    name='django-mixinvideos',
+    version='0.1.1',
+    url='https://github.com/edussilva/django-mixinvideos',
+    author='Eduardo Silva',
+    author_email='edussilva@live.com',
+    keywords='django mixins mixin video videos',
+    descriptions='Mixin for videos',
+    packages=['django-mixinvideos'],
+    isntall_requires=['requests'],
+)
