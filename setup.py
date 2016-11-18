32c90943705c67576589c1d29cee7712b60ac665
edussilva <edussilva@live.com>
2016-11-18 20:09:22 -0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup
 
 setup(
     name='django-mixinvideos',
-    version='0.1.1',
+    version='0.1.2',
     url='https://github.com/edussilva/django-mixinvideos',
     author='Eduardo Silva',
     author_email='edussilva@live.com',
     keywords='django mixins mixin video videos',
-    descriptions='Mixin for videos',
+    description='Mixin for videos',
     packages=['mixin_videos'],
     isntall_requires=['requests'],
 )
